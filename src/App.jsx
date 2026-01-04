1a4a8bea65fb002c5f279d8ea593a74138fdffaa
josimuddin198 <josimweb0504@gmail.com>
2026-01-04 23:46:28 +0600

    add initial layout with simple navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+
+function App() {
+  return (
+    <div className="w-11/12 mx-auto">
+      <header>
+        <Navbar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
